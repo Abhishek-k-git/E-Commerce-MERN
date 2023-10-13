c6f3b6692daefa2dcf1e2cfd6ceddf4002b2247d
Abhishek kumar <69133983+Abhishek-k-git@users.noreply.github.com>
2023-10-13 14:13:14 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -74,7 +74,8 @@ opts.secretOrKey = process.env.JWT_SECRET_KEY;
 
 //middlewares
 
-server.use(express.static(path.resolve(__dirname, 'build')));
+// server.use(express.static(path.resolve(__dirname, 'build')));
+server.use(express.static(path.join(__dirname, "/frontend/build")));
 server.use(cookieParser());
 server.use(
   session({
@@ -102,7 +103,8 @@ server.use('/orders', isAuth(), ordersRouter.router);
 
 // this line we add to make react router work in case of other routes doesnt match
 server.get('*', (req, res) =>
-  res.sendFile(path.resolve('build', 'index.html'))
+  res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
+  // res.sendFile(path.resolve('build', 'index.html'))
 );
 
 // Passport Strategies
