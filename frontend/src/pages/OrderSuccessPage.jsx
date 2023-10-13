751b9aeae352bca641c93107872fb4636dc3d328
Abhishek kumar <69133983+Abhishek-k-git@users.noreply.github.com>
2023-10-13 19:30:31 +0530

    Update OrderSuccessPage.jsx

--- frontend/src/pages/OrderSuccessPage.jsx
+++ frontend/src/pages/OrderSuccessPage.jsx
@@ -19,24 +19,20 @@ function OrderSuccessPage() {
   return (
     <>
     {!params.id &&  <Navigate to='/' replace={true}></Navigate>}
-    <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
-      <div className="text-center">
-        <p className="text-base font-semibold text-indigo-600">Order Successfully Placed</p>
-        <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
-          Order Number #{params?.id}
-        </h1>
-        <p className="mt-6 text-base leading-7 text-gray-600">
-          You can check your order in My Account &gt; My Orders
-        </p>
-        <div className="mt-10 flex items-center justify-center gap-x-6">
-          <Link
-            to="/"
-            className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-            Go back home
-          </Link>
-        </div>
+    <main className="w-screen h-screen bg-white flex items-center justify-center">
+    <div class="bg-white p-6 shadow-xl rounded-xl flex justify-center items-center flex-col">
+      <div className=""><img className="h-16 w-16" src="https://img.icons8.com/color/28/000000/check--v2.gif" /></div>
+      <div class="text-center">
+          <h3 class="md:text-base text-xs text-green-600 font-semibold text-center">Order Placed Successfully</h3>
+          <h2 className="text-gray-700 my-2 font-semibold text-base">Order Number #{params?.id}</h2>
+          <p class="text-gray-600 my-2">Thank you for completing your order</p>
+          <div class="py-10 text-center">
+              <Link to='/' class="px-8 bg-indigo-600 hover:bg-indigo-500 text-white text-sm font-semibold py-2 rounded-md">
+                  GO BACK 
+              </Link>
+          </div>
       </div>
+    </div>
     </main>
     </>
   );
